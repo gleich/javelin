d8cdf67f5d54f80fbdc7a7556d898d145c188eda
Matt Gleich <git@mattglei.ch>
2021-12-17 17:56:44 -0500

    feat: input new tasks
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
+
 	const submit = (event) => {
 		if (event.key === 'Enter') {
 			console.log(`Adding ${newTaskName}`);
+			tasks.set([...$tasks, newTaskName]);
 			newTaskName = '';
 		}
 	};
 
 	let newTaskName = '';
+	const tasks = writable([]);
 </script>
 
 <svelte:head>
@@ -17,6 +21,14 @@
 	<div class="container">
 		<h1>javelin</h1>
 		<input type="text" placeholder="New task" on:keydown={submit} bind:value={newTaskName} />
+		<div class="tasks">
+			{#each $tasks as task}
+				<div class="task">
+					<input type="checkbox" name={task} value={task} />
+					<label for={task}>{task}</label>
+				</div>
+			{/each}
+		</div>
 	</div>
 </main>
 
@@ -31,10 +43,6 @@
 		justify-content: center;
 	}
 
-	.container {
-		min-width: 40vw;
-	}
-
 	h1 {
 		font-size: 5rem;
 	}
@@ -45,5 +53,20 @@
 		font-size: 1rem;
 		border: none;
 		border-bottom: 1px solid var(--foreground-color);
+	}
+
+	.container {
+		min-width: 40vw;
+	}
+
+	.tasks {
+		display: flex;
+		flex-direction: column;
+		padding-top: 30px;
+	}
+
+	.task {
+		display: flex;
+		gap: 5px;
 	}
 </style>
